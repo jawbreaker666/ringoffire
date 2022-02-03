874a00f54533b0dfcdd162c88329c83c1845dd0d
jawbreaker666 <andrasev96@web.de>
2022-02-03 08:45:57 +0100

    first_finish

--- src/app/game-info/game-info.component.ts
+++ src/app/game-info/game-info.component.ts
@@ -16,9 +16,9 @@ export class GameInfoComponent implements OnInit, OnChanges {
     { title: 'Chicks', description: 'All girls drink!'},
     { title: 'Heaven', description: 'Put your hands up! The last player drinks!'},
     { title: 'Mate', description: 'Pick a mate. Your mate must always drink when you drink and the other way around.'},
-    { title: 'Thumbmaster', description: ''},
+    { title: 'Shots *3', description: 'Drink 3 Shots!'},
     { title: 'Men', description: 'All men drink!'},
-    { title: 'Quizmaster', description: ''},
+    { title: 'Expose', description: 'You have to miss a round'},
     { title: 'Never have i ever...', description: 'Say something you never did. Everyone who did it has to drink.'},
     { title: 'Rule', description: 'Make a rule. Everyone needs to drink when he breaks the rule.'},
     
